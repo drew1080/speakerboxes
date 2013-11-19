75fdb7c772a54b9b7bb70c6b3adecad02b0ef737
Andrew Cobb <cobb.andrew@gmail.com>
2013-11-19 10:24:09 -0500

    Adding validation to dropdown changes

--- wp-content/themes/gleam-child-theme/js/ise.js
+++ wp-content/themes/gleam-child-theme/js/ise.js
@@ -15,11 +15,29 @@
 }(jQuery));
 
 function validateForm(e) {
+  var form_submit = event.target.name == "submit" ? true : false;
+  
+  if ( form_submit ) {
+    processForm(e);
+  } else {
+    processDropDown(e);
+  }
+}
+
+function processForm(e){
   var form_valid = true;
+  
   jQuery(e.target).parents('form').find('select').each(function () {
+    var current_select_id = jQuery(this).attr('id');
+    
     if (jQuery(this).find(":selected").val() == "*") {
-      jQuery(this).after('<span class="form-errors" style="color:red"> ** Please Select **</span>');
+      if ( jQuery('.form-errors.' + current_select_id).length == 0 ) {
+        displayError(this, current_select_id);
+      }
+      
       form_valid = false;
+    } else {
+      clearError(current_select_id);
     }
   });
   
@@ -28,4 +46,21 @@ function validateForm(e) {
   } else {
     jQuery('.form-errors').html('');
   }
+}
+
+function processDropDown(e){
+  var current_select_id = jQuery(e.target).attr('id');
+  
+  if (jQuery(e.target).find(":selected").val() != "*") {
+    clearError(current_select_id);
+  }
+}
+
+function displayError(current_element, current_select_id){
+  jQuery(current_element).after('<span class="form-errors ' + current_select_id + 
+    '" style="color:red"> ** Please Select **</span>');
+}
+
+function clearError(current_select_id){
+  jQuery('.form-errors.' + current_select_id).remove();
 }
